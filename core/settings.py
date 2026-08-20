19343d9705e09b02252405cdec09ec858a654c79
iagomlkplay <iagofelicha@gmail.com>
2026-08-20 16:05:45 -0300

    Adicionar hosts permitidos para desenvolvimento no settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(cnwsqwd$o6%t!%=fdpv3kbelsnfngwu6&!ah*bicjbcavt_7c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
 
 # Application definition
