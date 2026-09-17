f2fc0e47b17e98b1783420f0ac6611719c18b1cd
iagomlkplay <iagofelicha@gmail.com>
2026-09-17 00:21:12 -0300

    Adicionar rotas para as páginas de reservas e nova reserva

--- reservas/urls.py
+++ reservas/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Páginas (HTML)
+    path('', views.index, name='index'),
+    path('nova-reserva/', views.nova_reserva_page, name='nova_reserva_page'),
+    path('reservas/', views.lista_reservas_page, name='lista_reservas_page'),
+
+    # API (JSON)
     path('api/reservas/', views.listar_reservas, name='listar_reservas'),
     path('api/reservas/criar/', views.criar_reserva, name='criar_reserva'),
 ]
