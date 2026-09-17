2330da38d07a8f4c8395a1cee4b157deb7ec33c5
iagomlkplay <iagofelicha@gmail.com>
2026-09-17 01:01:43 -0300

    Adicionar configurações de redirecionamento para login e logout

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,10 @@ STATIC_URL = 'static/'
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'reservas', 'static')]
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
